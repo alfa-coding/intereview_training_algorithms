402ed4e65ef9e712f823e8c036c163fd83a512c0
DimasVeliz <trevor90094@yandex.com>
2022-08-19 18:06:54 +0200

    Working on TRIE tree

--- implementations/dotnet/Algorithms.Library/TRIETree.cs
+++ implementations/dotnet/Algorithms.Library/TRIETree.cs
@@ -4,8 +4,30 @@ using System.Collections.Generic;
 
 namespace Algorithms.Library
 {
+    class TRIENode
+    {
+        public char Character { get; set; }
+
+        public TRIENode()
+        {
+            this.Character = '$';
+            this.Children = new TRIENode[26];
+        }
+        public TRIENode(char character)
+        {
+            this.Character = character;
+            this.Children = new TRIENode[26];
+        }
+
+        public TRIENode[] Children { get; set; }
+    }
     public class TRIETree<T> : IEnumerable<T>, IEnumerable
     {
+        private TRIENode root;
+        public TRIETree()
+        {
+            root = new TRIENode();
+        }
         public IEnumerator<T> GetEnumerator()
         {
             throw new NotImplementedException();
