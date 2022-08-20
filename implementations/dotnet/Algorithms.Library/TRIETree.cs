1878e939904d651af6291608a0876e80a075a219
DimasVeliz <trevor90094@yandex.com>
2022-08-20 21:06:08 +0200

    Config files, restored for debug

--- implementations/dotnet/Algorithms.Library/TRIETree.cs
+++ implementations/dotnet/Algorithms.Library/TRIETree.cs
@@ -4,13 +4,34 @@ using System.Collections.Generic;
 
 namespace Algorithms.Library
 {
+    class TrieEnumerator : IEnumerator
+    {
+        private TRIENode root;
+
+        public TrieEnumerator(TRIENode root)
+        {
+            this.root = root;
+        }
+
+        public object Current => throw new System.NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
     class TRIENode
     {
         public char Character { get; set; }
 
         public TRIENode()
         {
-            this.Character = '$';
+            this.Character = '^';
             this.Children = new TRIENode[26];
         }
         public TRIENode(char character)
@@ -20,22 +41,66 @@ namespace Algorithms.Library
         }
 
         public TRIENode[] Children { get; set; }
+        public bool IsEnd { get; internal set; }
     }
-    public class TRIETree<T> : IEnumerable<T>, IEnumerable
+    public class TRIETree : IEnumerable
     {
         private TRIENode root;
         public TRIETree()
         {
             root = new TRIENode();
         }
-        public IEnumerator<T> GetEnumerator()
+
+        public void AddWord(string literal)
         {
-            throw new NotImplementedException();
+            int index = 0;
+            TRIENode tmp = root;
+
+            while (index < literal.Length)
+            {
+                if (tmp.Children[literal[index] - 97] is null)
+                {
+                    TRIENode child = new TRIENode(literal[index]);
+                    tmp.Children[literal[index] - 97] = child;
+                }
+                tmp = tmp.Children[literal[index] - 97];
+
+                index++;
+
+            }
+
+            tmp.IsEnd = true;
         }
+
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new TrieEnumerator(root);
+        }
+
+        public List<string> ListAlphabetically()
+        {
+            List<string> response = new List<string>();
+            ListAlphabeticallyHelper(root, response, "");
+            return response;
+        }
+
+        private void ListAlphabeticallyHelper(TRIENode current, List<string> response, string forming)
+        {
+            if (current.IsEnd)
+            {
+                response.Add(forming);
+            }
+            if (current.Children is not null)
+            {
+                foreach (var children in current.Children)
+                {
+                    if (children is not null)
+                    {
+                        ListAlphabeticallyHelper(children,response,forming+children.Character);
+                    }
+                }
+            }
         }
     }
 }
