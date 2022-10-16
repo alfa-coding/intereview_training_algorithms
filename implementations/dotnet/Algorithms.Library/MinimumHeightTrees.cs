71e008c063fb9de4c51d67aff9752f7465566f8d
DimasVeliz <trevor90094@yandex.com>
2022-10-16 18:06:48 +0200

    Topological Sort for Minimu Height Tres, ->medium for leetcode ->hard to me

--- implementations/dotnet/Algorithms.Library/MinimumHeightTrees.cs
+++ implementations/dotnet/Algorithms.Library/MinimumHeightTrees.cs
@@ -8,6 +8,61 @@ namespace Algorithms.Library
     {
         SortedDictionary<int, int> hs = new();
 
+        public IList<int> FindMinHeightTrees(int n, int[][] edges)
+        {
+            if (n < 2)
+            {
+                List<int> centroids = new();
+                for (int i = 0; i < n; i++)
+                    centroids.Add(i);
+                return centroids;
+            }
+            Dictionary<int, HashSet<int>> adjList = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                adjList.Add(i, new HashSet<int>());
+            }
+            for (int i = 0; i < edges.Length; i++)
+            {
+                adjList[edges[i][0]].Add(edges[i][1]);
+                adjList[edges[i][1]].Add(edges[i][0]);
+            }
+
+            Queue<int> ceros = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (adjList[i].Count == 1)
+                {
+                    ceros.Enqueue(i);
+                }
+            }
+            int processedNodes = n;
+            List<int> lastLevel = new();
+
+            while (processedNodes > 2)
+            {
+                int size = ceros.Count;
+                processedNodes -= size;
+                for (int i = 0; i < size; i++)
+                {
+                    int curr = ceros.Dequeue();
+                    foreach (int next in adjList[curr])
+                    {
+                        adjList[next].Remove(curr);
+                        if (adjList[next].Count == 1)
+                            ceros.Enqueue(next);
+                    }
+                }
+            }
+            while (ceros.Count > 0)
+                lastLevel.Add(ceros.Dequeue());
+            return lastLevel;
+
+        }
+
+
         private int Heigth(Dictionary<int, HashSet<int>> adjList, int source, HashSet<int> visited)
         {
             if (adjList[source].Count == 0) return 1;
@@ -28,7 +83,7 @@ namespace Algorithms.Library
 
             return 1 + maxHeight;
         }
-        public IList<int> FindMinHeightTrees(int n, int[][] edges)
+        public IList<int> FindMinHeightTreesN2(int n, int[][] edges)
         {
 
             Dictionary<int, HashSet<int>> adjList = new();
